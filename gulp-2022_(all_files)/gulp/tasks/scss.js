f8e767acf7e89f282e837c011f3fc2c239951aa7
Yurii Tern <ternxstyle@gmail.com>
2022-01-13 22:49:21 +0200

    added_folder_gulp-2022

---
+++ gulp-2022_(all_files)/gulp/tasks/scss.js
@@ -0,0 +1,79 @@
+import dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+import rename from 'gulp-rename'
+import cleanCss from 'gulp-clean-css' // Сжатие CSS файла
+import webpcss from 'gulp-webpcss' // Вывод WEBP изображений
+import autoprefixer from 'gulp-autoprefixer' // Добавление вендорных префиксов
+import groupCssMediaQueries from 'gulp-group-css-media-queries' // Добавление вендорных префиксов
+
+const sass = gulpSass(dartSass)
+
+export const scss = () => {
+	return (
+		app.gulp
+			.src(app.path.src.scss, { sourcemaps: app.isDev })
+
+			.pipe(
+				app.plugins.plumber(
+					app.plugins.notify.onError({
+						title: 'SCSS',
+						message: 'Error: <%= error.message %>',
+					})
+				)
+		)
+
+			.pipe(app.plugins.replace(/@img\//g, '../img/'))
+
+			.pipe(
+				sass({
+					outputStyle: 'expanded',
+				}))
+
+			.pipe(
+				app.plugins.if(
+				app.isBuild,
+				groupCssMediaQueries()
+			)
+		)
+
+			.pipe(
+				app.plugins.if(
+					app.isBuild,
+					autoprefixer({
+						grid: true,
+						overrideBrowserslist: ['last 3 versions'],
+						cascade: true
+					})
+				)
+		)
+
+			.pipe(
+				app.plugins.if(
+					app.Build,
+					webpcss(
+						{
+							webpcss: '.webp',
+							noWebpClass: '.no-webp'
+						}
+					)
+				)
+			)
+
+			// Раскоментировать, если нужен не сжатый дубль файлп стилей
+			.pipe(app.gulp.dest(app.path.build.css))
+
+			.pipe(
+				app.plugins.if(
+					app.isBuild,
+					cleanCss()
+				)
+			)
+
+			.pipe(rename({
+					extname: '.min.css',
+			}))
+			
+			.pipe(app.gulp.dest(app.path.build.css))
+			.pipe(app.plugins.browsersync.stream())
+	)
+}
