e48bad8680518dfce535c47b741b39e1d5661318
Yurii Tern <ternxstyle@gmail.com>
2022-01-31 02:51:50 +0200

    Added htmlmin plugin + settings

---
+++ gulp-2022_htmlmin/gulp/tasks/html.js
@@ -0,0 +1,60 @@
+import fileInclude from 'gulp-file-include'
+import webpHtmlNosvg from 'gulp-webp-html-nosvg'
+import versionNumber from 'gulp-version-number'
+import htmlmin from 'gulp-htmlmin'
+
+// For PUG
+//import pug from "gulp-pug"
+
+export const html = () => {
+	return (
+		app.gulp
+			.src(app.path.src.html)
+			.pipe(
+				app.plugins.plumber(
+					app.plugins.notify.onError({
+						title: 'HTML',
+						message: 'Error: <%= error.message %>',
+					})
+				)
+			)
+			.pipe(fileInclude())
+			// For PUG
+			//.pipe(pug({
+			// Сжатие HTML файла
+			//pretty: true,
+			// Показывать в терминале какой файл обработан
+			//verbose: true
+			//}))
+			.pipe(app.plugins.replace(/@img\//g, 'img/'))
+
+			.pipe(app.plugins.if(app.isBuild, webpHtmlNosvg()))
+
+			.pipe(
+				htmlmin({
+					collapseWhitespace: true,
+					removeComments: true,
+				})
+			)
+
+			.pipe(
+				app.plugins.if(
+					app.isBuild,
+					versionNumber({
+						value: '%DT%',
+						append: {
+							key: '_v',
+							cover: 0,
+							to: ['css', 'js'],
+						},
+						output: {
+							file: 'gulp/version.json',
+						},
+					})
+				)
+			)
+
+			.pipe(app.gulp.dest(app.path.build.html))
+			.pipe(app.plugins.browsersync.stream())
+	)
+}
